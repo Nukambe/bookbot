957ea6c201e2bfc24367d8a5be587b10a0dd718d
nukambe <chappellwesley@gmail.com>
2024-10-13 15:25:30 -0400

    complete bookbot

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+def read_book(file):
+    with open(file) as book:
+        return book.read()
+
+def word_count(book):
+    words = book.split()
+    return len(words)
+
+def character_count(book: str):
+    characters: dict[str, int] = {}
+    for character in [character for character in book]:
+        if not character.isalpha(): continue
+        if character.lower() in characters:
+            characters[character.lower()] += 1
+        else:
+            characters[character.lower()] = 1
+    return characters
+
+def character_report(file: str, characters: dict[str, int]):
+    keys = sorted(characters.keys())
+    print(f"--- Begin report of {file} ---")
+    for key in keys:
+        print(f"The '{key}' character was found {characters[key]} times")
+    print("--- End report ---")
+
+if __name__ == '__main__':
+    book = read_book("books/frankenstein.txt")
+    count = word_count(book)
+    characters = character_count(book)
+    print(count)
+    print(characters)
+    report = character_report("books/frankenstein.txt", characters)
